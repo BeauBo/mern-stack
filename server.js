dd404ffe3cdb55b092160613857e254c218c7ee4
Bo Huang <bo.huang@paytm.com>
2019-12-08 21:51:02 -0500

    prepare deploy to heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
 
 const items = require('./routes/api/items')
 
@@ -18,6 +19,17 @@ mongoose.connect(db).then(() => console.log('MongoDB Connected')).catch(err => c
 
 // Use Routes
 app.use('/api/items', items)
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
